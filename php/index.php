612e8a4c759b008f9821591942be3c7f2bd29736
Sandrine Pichla <sandrinepichla@hotmail.com>
2022-06-30 18:03:25 +0200

    Modif inf.php

---
+++ php/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Ma page web</title>
+    </head>
+    <body>
+
+        <h1>Ma page web</h1>
+        <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); // la fonction affiche l'heure ?>.</p> 
+
+        <?php echo "On peut voir que ça fonctionne"; /* si je veux écrire un commentaire plus long,
+        donc sur plusieurs lignes, je fais comme ça.
+        Le code PHP ne se voit pas dans le code source de la page  */ ?>
+    
+    </body>
+</html>
